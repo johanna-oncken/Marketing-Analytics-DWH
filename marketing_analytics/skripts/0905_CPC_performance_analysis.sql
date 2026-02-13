b3477fa869b84660ae0c8d4f0efe555991714807
johanna-oncken <joh.oncken@gmail.com>
2026-02-13 08:46:34 +0100

    Refactor CPC analysis from campaigns to channels

--- marketing_analytics/skripts/0905_CPC_performance_analysis.sql
+++ marketing_analytics/skripts/0905_CPC_performance_analysis.sql
@@ -1,3 +1,4 @@
+
 /*
 ===============================================================================
 CPC Cost per Click Performance Analysis (Month-over-Month)
@@ -15,12 +16,12 @@ SQL Functions Used:
 Queries: 
     1) CPC overall 
     2) CPC Performance monthly
-    3.1) Monthly CPC Performance and MOM Analysis by Campaigns 
-    3.2) Top Ten Campaigns by Monthly CPC Improvements 
-    3.3) Top Ten Campaigns by Monthly CPC Declines 
-    4.1) Monthly CPC Performance and MOM Analysis by Channels 
-    4.2) Top Ten Channels by Monthly CPC Improvements 
-    4.3) Top Ten Channels by Monthly CPC Declines
+    3.1) Monthly CPC Performance and MOM Analysis by Channels 
+    3.2) Top Ten Channels by Monthly CPC Improvements 
+    3.3) Top Ten Channels by Monthly CPC Declines
+    4.1) Monthly CPC Performance and MOM Analysis by Campaigns 
+    4.2) Top Ten Campaigns by Monthly CPC Improvements 
+    4.3) Top Ten Campaigns by Monthly CPC Declines 
 
 ===============================================================================
 */
@@ -115,10 +116,114 @@ GO
 
 /*
 ===============================================================================
-3) CAMPAIGNS
+3) CHANNELS
 ===============================================================================
 */
 -- 3.1) MoM by Monthly Ad Spend and Clicks
+--      Analyze Month-over-Month CPC channel performance 
+DROP VIEW IF EXISTS gold.channels_cpc;
+GO
+
+CREATE VIEW gold.channels_cpc AS
+WITH monthly_clicks AS (
+SELECT
+    DATEFROMPARTS(d.year, d.month, 1) AS performance_month, 
+    f.click_channel, 
+    COUNT(f.click_id) AS clicks
+FROM gold.fact_clicks f
+LEFT JOIN gold.dim_date d 
+ON f.date_key = d.date_key
+WHERE f.click_channel IS NOT NULL
+GROUP BY DATEFROMPARTS(d.year, d.month, 1), f.click_channel
+), 
+monthly_spend AS (
+SELECT 
+    DATEFROMPARTS(YEAR(spend_date), MONTH(spend_date), 1) AS performance_month,
+    channel, 
+    SUM(spend) AS current_spend 
+FROM gold.fact_spend 
+WHERE channel IS NOT NULL 
+GROUP BY DATEFROMPARTS(YEAR(spend_date), MONTH(spend_date), 1), channel
+), 
+cpc_metrics AS (
+SELECT
+    COALESCE(c.performance_month, s.performance_month) AS performance_month,
+    c.click_channel, 
+    c.clicks AS current_clicks,
+    s.current_spend,
+    s.current_spend/NULLIF(c.clicks, 0) AS cpc
+FROM monthly_clicks c
+FULL JOIN monthly_spend s 
+ON c.performance_month = s.performance_month 
+    AND c.click_channel = s.channel
+)
+
+SELECT 
+    performance_month,
+    click_channel, 
+    current_spend,
+    current_clicks,
+    cpc AS current_cpc,
+    AVG(cpc) OVER(PARTITION BY click_channel) AS avg_cpc,
+    (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) AS diff_avg,
+    CASE 
+        WHEN (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) > 0 THEN 'Worsened (Above Avg)'
+        WHEN (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) < 0 THEN 'Improved (Below Avg)' 
+        ELSE 'Equals Average'
+    END AS avg_change,
+    -- Month-over_Month Analysis 
+    LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) AS pm_cpc, 
+    (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) AS diff_pm_cpc,
+    CASE 
+        WHEN (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) > 0 THEN 'Worsened (Higher)'
+        WHEN (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) < 0 THEN 'Improved (Lower)'
+        ELSE 'No Change'
+    END AS pm_change,
+    ROUND(
+        CASE 
+            WHEN LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) = 0 THEN NULL 
+            ELSE ((cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month))/LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month)*100 
+        END
+    ,2) AS mom_percentage
+FROM cpc_metrics
+WHERE click_channel IS NOT NULL;
+GO
+
+SELECT *
+FROM gold.channels_cpc
+ORDER BY click_channel, performance_month;
+GO
+
+-- 3.2) Top 10 Improvements MoM CPC channels
+SELECT TOP 10 *
+FROM gold.channels_cpc
+WHERE diff_pm_cpc IS NOT NULL
+ORDER BY diff_pm_cpc ASC; 
+
+SELECT TOP 10 *
+FROM gold.channels_cpc
+WHERE pm_cpc IS NOT NULL
+ORDER BY mom_percentage ASC;
+
+-- 3.3) Top 10 Declines MoM cpc channels
+SELECT TOP 10 *
+FROM gold.channels_cpc
+WHERE diff_pm_cpc IS NOT NULL
+ORDER BY diff_pm_cpc DESC; 
+
+SELECT TOP 10 *
+FROM gold.channels_cpc
+WHERE pm_cpc IS NOT NULL
+ORDER BY mom_percentage DESC;
+
+
+
+/*
+===============================================================================
+4) CAMPAIGNS
+===============================================================================
+*/
+-- 4.1) MoM by Monthly Ad Spend and Clicks
 --      Analyze Month-over-Month CPC campaign performance 
 DROP VIEW IF EXISTS gold.campaigns_cpc;
 GO
@@ -198,7 +303,7 @@ FROM gold.campaigns_cpc
 ORDER BY campaign_id, performance_month;
 GO
 
--- 3.2) Top 10 Improvements MoM CPC campaigns
+-- 4.2) Top 10 Improvements MoM CPC campaigns
 SELECT TOP 10 *
 FROM gold.campaigns_cpc
 WHERE diff_pm_cpc IS NOT NULL
@@ -209,7 +314,7 @@ FROM gold.campaigns_cpc
 WHERE pm_cpc IS NOT NULL
 ORDER BY mom_percentage ASC;
 
--- 3.3) Top 10 Declines MoM CPC campaigns
+-- 4.3) Top 10 Declines MoM CPC campaigns
 SELECT TOP 10 *
 FROM gold.campaigns_cpc
 WHERE diff_pm_cpc IS NOT NULL
@@ -217,109 +322,6 @@ ORDER BY diff_pm_cpc DESC;
 
 SELECT TOP 10 *
 FROM gold.campaigns_cpc
-WHERE pm_cpc IS NOT NULL
-ORDER BY mom_percentage DESC;
-
-
-/*
-===============================================================================
-4) CHANNELS
-===============================================================================
-*/
--- 4.1) MoM by Monthly Ad Spend and Clicks
---      Analyze Month-over-Month CPC channel performance 
-DROP VIEW IF EXISTS gold.channels_cpc;
-GO
-
-CREATE VIEW gold.channels_cpc AS
-WITH monthly_clicks AS (
-SELECT
-    DATEFROMPARTS(d.year, d.month, 1) AS performance_month, 
-    f.click_channel, 
-    COUNT(f.click_id) AS clicks
-FROM gold.fact_clicks f
-LEFT JOIN gold.dim_date d 
-ON f.date_key = d.date_key
-WHERE f.click_channel IS NOT NULL
-GROUP BY DATEFROMPARTS(d.year, d.month, 1), f.click_channel
-), 
-monthly_spend AS (
-SELECT 
-    DATEFROMPARTS(YEAR(spend_date), MONTH(spend_date), 1) AS performance_month,
-    channel, 
-    SUM(spend) AS current_spend 
-FROM gold.fact_spend 
-WHERE channel IS NOT NULL 
-GROUP BY DATEFROMPARTS(YEAR(spend_date), MONTH(spend_date), 1), channel
-), 
-cpc_metrics AS (
-SELECT
-    COALESCE(c.performance_month, s.performance_month) AS performance_month,
-    c.click_channel, 
-    c.clicks AS current_clicks,
-    s.current_spend,
-    s.current_spend/NULLIF(c.clicks, 0) AS cpc
-FROM monthly_clicks c
-FULL JOIN monthly_spend s 
-ON c.performance_month = s.performance_month 
-    AND c.click_channel = s.channel
-)
-
-SELECT 
-    performance_month,
-    click_channel, 
-    current_spend,
-    current_clicks,
-    cpc AS current_cpc,
-    AVG(cpc) OVER(PARTITION BY click_channel) AS avg_cpc,
-    (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) AS diff_avg,
-    CASE 
-        WHEN (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) > 0 THEN 'Worsened (Above Avg)'
-        WHEN (cpc) - AVG(cpc) OVER(PARTITION BY click_channel) < 0 THEN 'Improved (Below Avg)' 
-        ELSE 'Equals Average'
-    END AS avg_change,
-    -- Month-over_Month Analysis 
-    LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) AS pm_cpc, 
-    (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) AS diff_pm_cpc,
-    CASE 
-        WHEN (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) > 0 THEN 'Worsened (Higher)'
-        WHEN (cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) < 0 THEN 'Improved (Lower)'
-        ELSE 'No Change'
-    END AS pm_change,
-    ROUND(
-        CASE 
-            WHEN LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month) = 0 THEN NULL 
-            ELSE ((cpc) - LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month))/LAG(cpc) OVER(PARTITION BY click_channel ORDER BY performance_month)*100 
-        END
-    ,2) AS mom_percentage
-FROM cpc_metrics
-WHERE click_channel IS NOT NULL;
-GO
-
-SELECT *
-FROM gold.channels_cpc
-ORDER BY click_channel, performance_month;
-GO
-
--- 4.2) Top 10 Improvements MoM CPC channels
-SELECT TOP 10 *
-FROM gold.channels_cpc
-WHERE diff_pm_cpc IS NOT NULL
-ORDER BY diff_pm_cpc ASC; 
-
-SELECT TOP 10 *
-FROM gold.channels_cpc
-WHERE pm_cpc IS NOT NULL
-ORDER BY mom_percentage ASC;
-
--- 4.3) Top 10 Declines MoM cpc channels
-SELECT TOP 10 *
-FROM gold.channels_cpc
-WHERE diff_pm_cpc IS NOT NULL
-ORDER BY diff_pm_cpc DESC; 
-
-SELECT TOP 10 *
-FROM gold.channels_cpc
 WHERE pm_cpc IS NOT NULL
 ORDER BY mom_percentage DESC;
 
