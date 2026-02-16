8d7f0c338b5a52b57636b5081b68f03267bbe456
Johanna Oncken <joh.oncken@gmail.com>
2026-02-16 10:57:47 +0100

    Update and rename engagement_vs_conversion_v2.sql to .gitkeep

--- marketing_analytics/dashboard_views/engagement_vs_conversion_v2.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO TableName (channel, avg_click_intensity, click_intensity_level, avg_bofu_cvr, channel_profile)
-VALUES
-    ('Facebook Ads', 4,416125000000, 'High', 0,021975000000, 'Assist'),
-    ('Google Display', 4,301675000000, 'High', 0,022275000000, 'Assist'),
-    ('Google Search', 4,444750000000, 'High', 0,022600000000, 'Assist'),
-    ('Instagram Ads', 4,456125000000, 'High', 0,021350000000, 'Assist'),
-    ('TikTok Ads', 4,379200000000, 'High', 0,021025000000, 'Assist');
-
